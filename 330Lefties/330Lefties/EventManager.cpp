80534d23c52e9d0b8fcff4f32146231ca3f72ac3
stdunn <seantd@live.com>
2015-02-24 16:44:55 -0800

    Changed to temporary working version

--- 330Lefties/330Lefties/EventManager.cpp
+++ 330Lefties/330Lefties/EventManager.cpp
@@ -1,97 +1,64 @@
-#include <iostream>
-#include <sstream>
-#include <Windows.h>
-#include <SDL.h>
+#include <vector>
+#include "SDL.h"
+#include "GameObject.h"
 #include "EventManager.h"
-using namespace std;
 
-//void EventManager::Update(){
-std::string EventManager::Update(){
-	cout << "Updating" << endl;
+
+EventManager::~EventManager()
+{
+	eventQueue.clear();
+	subscriberMap.clear();
+}
+
+void EventManager::updateQueue()
+{
 	SDL_Event e;
-	bool receivedEvents = false;
-	std::string eventString = "NoEvent!";
-	while (SDL_PollEvent(&e)){
-		cout << "Getting Events!" << endl;
-		eventQueue.push(e);
-		receivedEvents = true;
-		/*
-		if (e.type == SDL_QUIT){
-		quit = true;
-		}
-		if (e.type == SDL_KEYDOWN){
-		if (e.key.keysym.sym == SDLK_RIGHT)
-		{
-		eventString = "walk right";
-		}
-		else if (e.key.keysym.sym == SDLK_LEFT)
-		{
-		eventString = "walk left";
-		}
-		else if (e.key.keysym.sym == SDLK_UP)
-		{
-		eventString = "walk up";
-		}
-		else if (e.key.keysym.sym == SDLK_DOWN)
-		{
-		eventString = "walk down";
-		quit = true;
-		}
-		}
-		*/
-		if (receivedEvents)
-			StartEventProcessing();// <-- TODO: make this actually process events after some time.
-	}
-	return eventString;
-}
 
-void EventManager::RegisterForEvent(Subscriber& subscriber, Uint32 theEvent){
-	cout << "RegEvent: " << theEvent << endl;
-	if (eventToListOfSubScribers.find(theEvent) == end(eventToListOfSubScribers)){//is there no list for this kind of event?
-		cout << "New Event to listen for, so create a new list." << endl;
-		SubscriberList newList;
-		newList.push_back(subscriber);
-		eventToListOfSubScribers.insert(std::pair<Uint32, SubscriberList>(theEvent, newList));
-	}
-	else{//seen this event before, so should have a list.
-		cout << "Seen this event before, adding to already made list!" << endl;
-		eventToListOfSubScribers[theEvent].push_back(subscriber);
+	while (SDL_PollEvent(&e))
+	{
+		eventQueue.insert(eventQueue.begin(), e);
 	}
 }
 
-void EventManager::DispatchEvent(Uint32 theEvent){
-	if (eventToListOfSubScribers.find(theEvent) != end(eventToListOfSubScribers)){//found the list
-		SubscriberList listForThisEvent = eventToListOfSubScribers[theEvent];
+void EventManager::subscribe(GameObject& const o, Uint32 eventType)
+{
+	std::map < Uint32, std::vector<GameObject* const>>::iterator it = subscriberMap.find(eventType);
 
-		for (Subscriber s : listForThisEvent){
-			cout << "trying to fire function for this event: " << s.eventID << endl;
-			//Wrapper_Int(&forwarder, &object);
-			//Wrapper_Int(&s.DefaultAction, &s.owner);
-		}
+	if (it == subscriberMap.end())
+	{
+		std::vector<GameObject* const> newSubscriberList;
+		newSubscriberList.push_back(&o);
 
+		subscriberMap.insert(std::pair<Uint32, std::vector<GameObject* const>>(eventType, newSubscriberList));
+	}
+	else
+	{
+		subscriberMap[eventType].push_back(&o);
 	}
 }
 
+void EventManager::handleEvents()
+{
+	if (eventQueue.size() == 0)
+	{
+		return;
+	}
 
-void EventManager::StartEventProcessing(){
+	Uint32 start = SDL_GetTicks();
 
-	while (eventQueue.size() > 0){
-		SDL_Event e = eventQueue.front();
-		eventQueue.pop();//for some reason the pop doesn't return the element.
+	while ((SDL_GetTicks() - start < timeToHandleEvents) && eventQueue.size() != 0)
+	{
+		SDL_Event e = eventQueue.back();
 
-		//Try to get the right enum for this event
-		Uint32 theEventEnum = e.type;//Start it by default as type
-		if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP){//If its a Key press, get that key enum
-			theEventEnum = e.key.keysym.sym;
-			cout << "was a key!!!!!!!!!!!!!!" << endl;
+		std::map < Uint32, std::vector<GameObject* const>>::iterator it = subscriberMap.find(e.type);
+
+		if (it != subscriberMap.end())
+		{
+			for (int i = 0; i < subscriberMap[e.type].size(); i++)
+			{
+				subscriberMap[e.type][i]->handleEvent(e);
+			}
 		}
-		else if (e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP){
-			theEventEnum = e.button.button;//Left, Right
-			cout << "Mouse click" << endl;
-		}
-		//else if (SDL_USEREVENT) <-- TODO!
-
-		cout << "theEventEnum: " << theEventEnum << endl;
-		DispatchEvent(theEventEnum);
+		eventQueue.pop_back();
 	}
 }
