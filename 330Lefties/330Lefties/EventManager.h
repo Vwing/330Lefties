8034823118c53d4ad4b0b756369a4e0444b458a5
stdunn <seantd@live.com>
2015-02-24 16:45:37 -0800

    Changed to temporary working version

--- 330Lefties/330Lefties/EventManager.h
+++ 330Lefties/330Lefties/EventManager.h
@@ -1,62 +1,35 @@
 #pragma once
 
-#include <iostream>
-#include <sstream>
-
 #include "SDL.h"
 #include <vector>
 #include <map>
-#include <deque>
-#include <queue>
-#include "Subscriber.h"
+#include "GameObject.h"
 
-
-
-//#include "Globals.h"
-
-typedef std::vector<Subscriber> SubscriberList;
-typedef std::map<Uint32, SubscriberList> MyMap;
 class EventManager
 {
 public:
-	//Necessary for a singleton!
 	static EventManager& getInstance()
 	{
-		static EventManager instance; // Guaranteed to be destroyed.
-		// Instantiated on first use.
+		static EventManager instance;
 		return instance;
 	}
 
-	std::string Update();
+	~EventManager();
 
-	//void RegisterForEvent(void* owner, std::string className, double eventToListenFor, SubScriptionFunction callBackFunction);
-	void RegisterForEvent(Subscriber& subscriber, Uint32 theEvent);
-	void DispatchEvent(Uint32 theEvent);
-	//static bool loadedOnce;
+	void updateQueue();
+	void subscribe(GameObject& const o, Uint32 type);
+	void handleEvents();
+
 private:
-	//std::deque<SDL_Event> eventQueue;//deque because  supports more features like for each
-	std::queue<SDL_Event> eventQueue;//deque because  supports more features like for each
+	EventManager() {};
 
-	//Necessary for a singleton!
-	EventManager(){};
-	EventManager(EventManager const&) = delete;
+	EventManager(EventManager const &) = delete;
 	void operator=(EventManager const&) = delete;
 
-	void StartEventProcessing();
-
-
-	/*
-
-	The eventToListOfSubScribers is a mapping of events to a list of all the Subscriptions for that Event.
-	Each Subscription contains the reference to the class and function to call on this event.
-
-	[SDLK_LEFT]->	[
-	[&ClassA]	-> "OnSDLK_LEFT_FromClassA",
-	[&ClassB]	-> "OnSDLK_LEFT_FromClassB",
-	}
-
-	*/
-	MyMap eventToListOfSubScribers;
-
+	// Time dedicated to handling events in handleEvents()
+	int timeToHandleEvents = 100;
+	std::vector<SDL_Event> eventQueue;
+	std::map < Uint32, std::vector<GameObject* const>> subscriberMap;
 };
+
 
