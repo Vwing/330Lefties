0ce026857b469857a6b1c96f81087e72fe025751
stdunn <seantd@live.com>
2015-02-24 16:48:33 -0800

    Primary Game object. Created once in the game loop.

---
+++ 330Lefties/330Lefties/Game.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "SDL.h"
+#include "EventManager.h"
+#include "GameObject.h"
+#include "SpriteWithBody.h"
+
+class Game
+{
+public:
+	Game(int windowWidth, int windowHeight, int xPos = 0, int yPos = 0);
+	~Game();
+
+	void update();
+	void render();
+
+	bool isOver();
+	// void addObject(GameObject* o);
+
+	Sprite* addSprite(int width, int height, int xPos = 0, int yPos = 0);
+	
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+
+private:
+	// Window Dimensions
+	int xPos, yPos;
+	int windowHeight, windowWidth;
+	bool quit;
+
+	std::vector<GameObject*> allGameObjects;
+
+};
