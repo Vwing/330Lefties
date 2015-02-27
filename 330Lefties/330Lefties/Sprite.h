92eb7ce2d447cdaa981bd3424b60c4abc5ee871a
stdunn <seantd@live.com>
2015-02-27 00:54:27 -0800

    Update and rename 330Lefties/330Lefties to 330Lefties/330Lefties/Sprite.h

---
+++ 330Lefties/330Lefties/Sprite.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "GameObject.h"
+#include <vector>
+#include <map>
+
+class Sprite : public GameObject
+{
+public:
+	Sprite(int width, int height, SDL_Renderer* ren, int xPos= 0, int yPos = 0, std::string start_seq = "");
+	~Sprite(void);
+
+	void setPos(int x, int y);
+	void movex(int delta);
+	void movey(int delta);
+	int getX();
+	int getY();
+
+	// makeFrame returns the unique index of the frame
+	int makeFrame(SDL_Texture* texture, int x, int y);
+
+	// addFrameToSequence returns the number of frames in the sequence after the add
+	int addFrameToSequence(std::string seqName, int frameIndex);
+
+	// show(int) renders the frame with the specified frameIndex
+	void show(int frameIndex);
+	// show(string) cycles through all frames in the specified sequence, one per call
+	void show(std::string sequence);
+	void changeSequence(std::string seq);
+
+	virtual void handleEvent(SDL_Event sdlEvent);
+	virtual void update();
+	virtual void render();
+
+protected:
+	SDL_Renderer* renderer;
+	std::string current_seq; // The name of the current sequence being displayed. Default is empty string.
+
+	struct frame
+	{
+		SDL_Texture* texture;
+		int x;
+		int y;
+	};
+	// Container for all the frames available to this sprite
+	std::vector<frame> frames;
+
+	// Mapping of sequence names to pairs of (1. List of frames 2. Current sequence index)
+	std::map<std::string, std::pair <std::vector<int>, unsigned int>> sequenceList;
+};
