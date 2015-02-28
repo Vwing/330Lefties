016d859eea536013edf1e980c71c972ddb79268a
erikgllgs <edgalleg@uci.edu>
2015-02-27 18:44:54 -0800

    updated physics to work with the GameObject class

--- 330Lefties/330Lefties/Physics.h
+++ 330Lefties/330Lefties/Physics.h
@@ -1,41 +1,39 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
+#include "SDL.h"
+#include "GameObject.h"
 
 class Physics{
 public:
-	int x, y, width, height;
-	double gravity = -1;
 
-	void setPos(int ex, int why){
-		x = ex;
-		y = why;
-	}
-
-	void set_width_height(int w, int h){
-		width = w;
-		height = h;
-	}
-
-	void check_collision(){
-		if (y + height > 480){ //bottom
-			y = 480 - height;
-			gravity = -1;
+	void check_borders(GameObject obj){ //constants can be replaced with variables
+		if (obj.body.yPos + obj.body.height > 480){ //bottom
+			obj.body.yPos = 480 - obj.body.height; 
 		}
-		else if (y < 0)  //top
-			y = 0;
-		if (x + width > 640)  //right
-			x -= width / 20;
-		else if (x < 0) //left
-		x = 0;
+		else if (obj.body.yPos < 0) //top
+			obj.body.yPos = 0;
+		if (obj.body.xPos + obj.body.width > 640) //right
+			obj.body.xPos -= obj.body.width / 20;
+		else if (obj.body.xPos < 0) //left
+			obj.body.xPos = 0;
 	}
 
-	void update(int& x, int& y){
-		gravity = gravity*1.04;
-		y -= gravity;
-		check_collision();
+	bool check_collision(GameObject obj1, GameObject obj2){
+		return !(obj2.body.xPos > obj1.body.xPos + obj1.body.width
+			|| obj2.body.xPos + obj2.body.width < obj1.body.xPos
+			|| obj2.body.yPos > obj1.body.yPos + obj1.body.height
+			|| obj2.body.yPos + obj2.body.height < obj1.body.yPos);
 	}
 
+
+	void gravity(GameObject obj/*game object*/){ //
+		obj.body.xPos -= 2;
+	}
+
+	void update(GameObject obj){
+		check_borders(obj);
+		//check_collision has to be called from somewhere else
+		gravity(obj);
+	}
 };
-
-
 #endif
