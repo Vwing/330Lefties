74995e1cad7075dc27b148d19d77bbe488bae51e
stdunn <seantd@live.com>
2015-03-02 14:51:08 -0800

    Camera:  Added comments for event implementation. Removed default values from constructor.

--- 330Lefties/330Lefties/Camera.h
+++ 330Lefties/330Lefties/Camera.h
@@ -1,16 +1,14 @@
 #pragma once
 
 #include "GameObject.h"
+#include "SDL.h"
 
 class Camera : public GameObject
 {
 public:
-	Camera(int width = 500, int height = 500, int xPos = 0, int yPos = 0);
+	Camera(int width, int height, int xPos = 0, int yPos = 0);
 
 	bool canSee(GameObject* obj1); //stolen from Physics
-
-	//SDL_Window *window;
-	//SDL_Renderer *renderer;
 
 	virtual void handleEvent(SDL_Event sdlEvent);
 	virtual void update();
