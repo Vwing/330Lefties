b751f3e3934fba8786854adc95de40e7fedb0257
stdunn <seantd@live.com>
2015-03-02 14:53:45 -0800

    Updated constructor. Added addObject() function.

--- 330Lefties/330Lefties/Environment.h
+++ 330Lefties/330Lefties/Environment.h
@@ -8,11 +8,17 @@
 class Environment
 {
 public:
-	Environment();
+	Environment(int worldWidth, int worldHeight, Camera* c);
 	void update();
+	// Might want a render() here
+
+	void addObject(GameObject* o);
 
 private:
+	int WORLD_WIDTH;
+	int WORLD_HEIGHT;
+
 	std::vector<GameObject*> environment;
 	Camera* camera;
-	Physics physics;
+	Physics* physics;
 };
