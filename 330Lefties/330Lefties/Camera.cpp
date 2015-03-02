bd128c3cc01a4e7e78b6ab68b93480f8b064a8bf
stdunn <seantd@live.com>
2015-03-02 14:51:49 -0800

    Added comments for event implementation. Removed default values from constructor.

--- 330Lefties/330Lefties/Camera.cpp
+++ 330Lefties/330Lefties/Camera.cpp
@@ -7,6 +7,8 @@ Camera::Camera(int width, int height, int xPos, int yPos)
 	body.xPos = xPos;
 	body.yPos = yPos;
 	body.physical = false;
+
+	// add subscriptions here
 }
 
 bool Camera::canSee(GameObject* obj1){ //stolen from Physics
@@ -20,4 +22,12 @@ void Camera::update() {}
 
 void Camera::render() {}
 
-void Camera::handleEvent(SDL_Event sdlEvent) {}
+void Camera::handleEvent(SDL_Event sdlEvent) 
+{
+	/*
+		Implementation for events:
+		
+		When and how should the camera move based on what input
+	
+	*/
+}
