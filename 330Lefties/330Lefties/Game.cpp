041a5bc9c60b55ff52e28495474b9bedd5900f38
stdunn <seantd@live.com>
2015-03-08 14:47:24 -0700

    Added SDL_init stuff; added setEnvironment, quitGame(); updated createSprite()

--- 330Lefties/330Lefties/Game.cpp
+++ 330Lefties/330Lefties/Game.cpp
@@ -1,48 +1,69 @@
 #include <iostream>
 #include "Game.h"
 #include "SDL.h"
+#include <SDL_image.h>
+
+#include <cleanup.h>
 
 
 Game::Game(int windowWidth, int windowHeight, int xPos, int yPos)
 : windowWidth(windowWidth), windowHeight(windowHeight), xPos(xPos), yPos(yPos)
 {
 	quit = false;
-	
-	// Environment will use window width and height as default until we implement scrolling
-	environment = new Environment(windowWidth, windowHeight);
-	camera = new Camera(windowWidth, windowHeight, environment);
 
-	// Load all resources
+	/*
+		Load all of the SDL resources and set quit to true if there are any errors
+	*/
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, SDL_GetError());
+		quit = true;
+	}
+
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG){
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "IMG_Init");
+		quit = true;
+	}
+
 	window = SDL_CreateWindow("Your Game", xPos, yPos, windowWidth,
 		windowHeight, SDL_WINDOW_SHOWN);
 	if (window == nullptr){
-		std::cout << "Create Window Error" << std::endl;
-		SDL_Quit();
-		// return 1;
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Create Window Error");
+		quit = true;
 	}
 	renderer = SDL_CreateRenderer(window, -1,
 		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == nullptr){
-		std::cout << "Create Renderer Error" << std::endl;
-		// cleanup(window);
-		SDL_Quit();
-		// return 1;
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Create Renderer Error");
+		quit = true;
 	}
+
+	/*	Environment will use window width and height as default
+		NOTE: For a platformer game, the environment should be wider (and possibly
+		higher than the window.
+	*/
+	environment = new Environment(windowWidth, windowHeight);
+	camera = new Camera(windowWidth, windowHeight, environment);
 }
 
 Game::~Game()
 {
-	// cleanup(window, renderer);
+	cleanup(window, renderer);
+	delete environment, camera;
+	IMG_Quit();
+	SDL_Quit();
 }
 
 void Game::update()
 {
-//	EventManager::getInstance().updateQueue();
-	//EventManager::getInstance().handleEvents();
+	//	EventManager::getInstance().updateQueue();
+	//  EventManager::getInstance().handleEvents();
 	EventManager::getInstance().Update();
 
+	// Position the camera based on the current mode
 	camera->update();
+	// Call update on all environment objects
 	environment->update();
+	// Call update on all UI objects
 	UI->update();
 }
 
@@ -60,6 +81,11 @@ void Game::render()
 	SDL_RenderPresent(renderer);
 }
 
+void Game::setEnvironment(Environment *newEnv)
+{
+	camera->setEnvironment(newEnv);
+}
+
 /*
 Sprite* Game::addSprite(int width, int height, int xPos, int yPos)
 {
@@ -71,9 +97,9 @@ Sprite* Game::addSprite(int width, int height, int xPos, int yPos)
 	return newSprite;
 }
 */
-Sprite* Game::createSprite(int width, int height, int xPos, int yPos)
+Sprite* Game::createSprite(std::string resPath, int width, int height, int xPos, int yPos)
 {
-	Sprite* newSprite = new Sprite(width, height, renderer, xPos, yPos);
+	Sprite* newSprite = new Sprite(resPath, renderer, width, height, xPos, yPos);
 	return newSprite;
 }
 
@@ -98,3 +124,9 @@ bool Game::isOver()
 {
 	return quit;
 }
+
+void Game::quitGame()
+{
+	quit = true;
+}
+
