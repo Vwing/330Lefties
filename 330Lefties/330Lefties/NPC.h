37f84e75487263b3105a67888651dda0b6eedbbc
atymisk <anthoc2@uci.edu>
2015-03-08 00:05:55 -0800

    Create NPC.h

---
+++ 330Lefties/330Lefties/NPC.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "Unit.h"
+#include "Sprite.h"
+
+class NPC : public Unit
+{
+public:
+	bool enableAI = true;//not sure how this works
+	bool enablePhysics = true;
+
+	//NPC();
+	//~NPC();
+	
+	virtual void GameObject::update() = 0;
+	virtual void GameObject::render() = 0;
+	virtual void GameObject::handleEvent(Uint32 event) = 0;
+};
