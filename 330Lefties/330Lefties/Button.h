700ad80d6bd6066d2d77a7f9089240b43d2e75f4
Vincent Tang <vincet3@uci.edu>
2015-03-08 19:17:39 -0700

    Update Button.h

--- 330Lefties/330Lefties/Button.h
+++ 330Lefties/330Lefties/Button.h
@@ -7,16 +7,26 @@
 #include "Sprite.h"
 #include "UI_Element.h"
 
+struct ButtonPress{
+	int buttonOver = 0;
+	int buttonUp = 1;
+	int buttonDown = 2;
+};
+
 class Button : public UI_Element{
 public:
-	Button(SDL_Renderer* ren, std::string resPath, std::string spritesheet, std::function<void(void)> functocall, Sprite* sprite);
-	~Button(void);
-	void logSDLError(std::ostream &os, const std::string &msg);
-	SDL_Texture* loadTexture();
+	Button(std::function<void(void)> functocall, Sprite* sprite);
 	void handleEvent(Uint32 sdlEvent);
+	void onButtonDown(int frameNum); // Change frame for Button Down
+	void onButtonUp(int frameNum); // Change frame for Button Up
+	void onButtonOver(int frameNum); // Change frame for Button Over
+	int getButtonDown(); // Get frame for Button Down
+	int getButtonUp(); // Get frame for Button Up
+	int getButtonOver(); // Get frame for Button Over
 private:
 	SDL_Renderer* renderer;
 	std::string resPath, spritesheet;
 	std::function<void(void)> functocall;
 	Sprite* sprite;
+	ButtonPress press;
 };
