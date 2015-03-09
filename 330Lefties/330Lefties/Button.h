6151dfbce0c0a629758bcebbc3c3628b86d1a384
Vincent Tang <vincet3@uci.edu>
2015-03-09 14:37:11 -0700

    Update Button.h

--- 330Lefties/330Lefties/Button.h
+++ 330Lefties/330Lefties/Button.h
@@ -3,30 +3,53 @@
 #include "SDL.h"
 #include <vector>
 #include <map>
-#include <functional>
-#include "Sprite.h"
-#include "UI_Element.h"
 
-struct ButtonPress{
-	int buttonOver = 0;
-	int buttonUp = 1;
-	int buttonDown = 2;
-};
-
-class Button : public UI_Element{
+class Sprite
+{
 public:
-	Button(std::function<void(void)> functocall, Sprite* sprite);
-	void handleEvent(Uint32 sdlEvent);
-	void onButtonDown(int frameNum); // Change frame for Button Down
-	void onButtonUp(int frameNum); // Change frame for Button Up
-	void onButtonOver(int frameNum); // Change frame for Button Over
-	int getButtonDown(); // Get frame for Button Down
-	int getButtonUp(); // Get frame for Button Up
-	int getButtonOver(); // Get frame for Button Over
+	Sprite(int width, int height, SDL_Renderer* ren);
+	~Sprite(void);
+
+	void setPos(int x, int y);
+	void movex(int delta);
+	void movey(int delta);
+	int getX();
+	int getY();
+
+	// makeFrame returns the unique index of the frame
+	int makeFrame(SDL_Texture* texture, int x, int y);
+
+	// addFrameToSequence returns the number of frames in the sequence after the add
+	int addFrameToSequence(std::string seqName, int frameIndex);
+
+	// show(int) renders the frame with the specified frameIndex
+	void show(int frameIndex);
+
+	// show(string) cycles through all frames in the specified sequence, one per call
+	void show(std::string sequence);
+
+	// takes in the sapce moved for x and y and the location of the boundary (height and width) and return true if they don't have collision and false if they do
+	bool bound(int movex, int movey, int Bheight, int Bwidth);
+
+	// The private part of the class is given as a hint or suggestion.
+	// In homework 3 you can make any modifications you want to the class's innards.
 private:
+	int width, height;
+	int currX, currY;		// the coordinates of the sprite's upper left hand corner relative to the window
 	SDL_Renderer* renderer;
-	std::string resPath, spritesheet;
-	std::function<void(void)> functocall;
-	Sprite* sprite;
-	ButtonPress press;
+
+	struct frame
+	{
+		SDL_Texture* texture;
+		int x;
+		int y;
+	};
+	std::vector<frame> frames;
+
+	std::map<std::string, std::vector<int>> sequenceList;
+	int sequenceIndex;		// shared by all sequences; it would be better to have
+	// one for each sequence
+
+
 };
+
