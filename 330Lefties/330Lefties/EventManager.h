7713ec6693b2554678f04006fc138fc93fe98eb1
BearArry <twmiller@uci.edu>
2015-03-09 17:54:58 -0700

    hupdating h

--- 330Lefties/330Lefties/EventManager.h
+++ 330Lefties/330Lefties/EventManager.h
@@ -23,7 +23,7 @@ public:
 		return instance;
 	}
 
-	std::string Update();
+	void Update();
 
 	void RegisterForEvent(Subscriber& subscriber, Uint32 theEvent);
 	void DispatchEvent(Uint32 theEvent);
@@ -53,42 +53,3 @@ private:
 	MyMap eventToListOfSubScribers;
 
 };
-
-
-
-/*#pragma once
-
-#include "SDL.h"
-#include <vector>
-#include <map>
-#include "GameObject.h"
-
-class EventManager
-{
-public:
-	static EventManager& getInstance()
-	{
-		static EventManager instance;
-		return instance;
-	}
-
-	~EventManager();
-
-	void updateQueue();
-	void subscribe(GameObject& const o, Uint32 type);
-	void handleEvents();
-
-private:
-	EventManager() {};
-
-	EventManager(EventManager const &) = delete;
-	void operator=(EventManager const&) = delete;
-
-	// Time dedicated to handling events in handleEvents()
-	int timeToHandleEvents = 100;
-	std::vector<SDL_Event> eventQueue;
-	std::map < Uint32, std::vector<GameObject* const>> subscriberMap;
-};
-
-
-*/
