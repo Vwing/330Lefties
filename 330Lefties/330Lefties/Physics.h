2a80d1699db2f0a7c3300625e9dd28f4d3fcd412
erikgllgs <edgalleg@uci.edu>
2015-03-10 15:41:04 -0700

    Update Physics.h
    Using magnitude to calculate gravity

--- 330Lefties/330Lefties/Physics.h
+++ 330Lefties/330Lefties/Physics.h
@@ -3,6 +3,7 @@
 #include "SDL.h"
 #include "Unit.h"
 #include <vector>
+#include <math.h> //I don't think this is needed, but it's here just in case
 
 class Physics{
 public:
@@ -11,67 +12,50 @@ public:
 	{
 	}
 
-	// Returns true if given game object is colliding with world bounds
-	// and moves the object back within the bounds. (<- should go in different function)
-	bool check_borders(Unit* obj){ //constants can be replaced with variables
-		if (obj->body.yPos + obj->body.height >= WORLD_HEIGHT){ //bottom
-			obj->body.yPos = WORLD_HEIGHT - obj->body.height;
-			return true;
-		}
-		else if (obj->body.yPos <= 0) //top
-		{
-			obj->body.yPos = 0;
-			return true;
-		}
-		if (obj->body.xPos + obj->body.width >= WORLD_WIDTH) //right
-		{
-			obj->body.xPos -= obj->body.width / 20;
-			return true;
-		}
-		else if (obj->body.xPos <= 0) //left
-		{
-			obj->body.xPos = 0;
-			return true;
-		}
-			
+bool check_borders(Unit* obj){
+	return (obj->body.yPos + obj->body.height >= WORLD_HEIGHT //bottom
+	|| obj->body.yPos <= 0 //top
+	|| obj->body.xPos + obj->body.width >= WORLD_WIDTH //right
+	|| obj->body.xPos <= 0) //left
 	}
 
-	void set_world(int width, int height)
+void set_world(int width, int height){
+	WORLD_HEIGHT = height;
+	WORLD_WIDTH = width;
+	}
+
+bool check_collision(Unit* obj1, Unit* obj2){
+	return !(obj2->body.xPos > obj1->body.xPos + obj1->body.width
+	|| obj2->body.xPos + obj2->body.width < obj1->body.xPos
+	|| obj2->body.yPos > obj1->body.yPos + obj1->body.height
+	|| obj2->body.yPos + obj2->body.height < obj1->body.yPos);
+	}
+
+int magnitude(Unit* obj){
+	return obj->width * obj->height;
+	}
+
+void gravity(Unit* obj){
+	obj->body.xPos += (log10(magnitude(obj)))/1.5;
+	}
+
+void update(Unit* obj){
+	check_borders(obj);
+	gravity(obj);
+	}
+
+void update(std::vector<Unit*> objects){
+	for (Unit* o : objects)
 	{
-		WORLD_HEIGHT = height;
-		WORLD_WIDTH = width;
+		for (Unit* p : objects)
+		check_collision(o, p);
+		check_borders(o);
+		gravity(o);
 	}
-
-	bool check_collision(Unit* obj1, Unit* obj2){
-		return !(obj2->body.xPos > obj1->body.xPos + obj1->body.width
-			|| obj2->body.xPos + obj2->body.width < obj1->body.xPos
-			|| obj2->body.yPos > obj1->body.yPos + obj1->body.height
-			|| obj2->body.yPos + obj2->body.height < obj1->body.yPos);
-	}
-
-	void gravity(Unit* obj){ 
-		// obj->body.xPos -= obj->body.gravity;
-		obj->body.xPos -= 1;
-	}
-
-	void update(Unit* obj){
-		check_borders(obj);
-		//check_collision has to be called from somewhere else
-		gravity(obj);
-	}
-
-	void update(std::vector<Unit*> objects){
-		for (Unit* o : objects)
-		{
-			for (Unit* p : objects)
-				check_collision(o, p);
-			check_borders(o);
-			gravity(o);
-		}
-	}
+}
 
 private:
-	int WORLD_WIDTH;
-	int WORLD_HEIGHT;
+int WORLD_WIDTH;
+int WORLD_HEIGHT;
 };
 #endif
