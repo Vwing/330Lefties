dd84ff8c329f3dee7c323d0b870da1f59d70e9b8
atymisk <anthoc2@uci.edu>
2015-03-13 01:13:36 -0700

    Adding AI element to the Unit header

--- 330Lefties/330Lefties/Unit.h
+++ 330Lefties/330Lefties/Unit.h
@@ -1,15 +1,24 @@
 #pragma once
 
 #include "GameObject.h"
-#include <vector>
-// #include "AI.h"
+#include "AI.h"
 
 // 'Unit' objects are non-UI objects that can have AI or physics actions.
 // They are stored in the environment
 class Unit : public GameObject
 {
 public:
-// AI* ai;
+	AI* ai;
 	bool enableAI = false;
 	bool enablePhysics = false;
+
+	AI* createAI(int x, int y)
+	{
+		if (enableAI)
+		{
+			ai = new AI(x, y);
+			return ai;
+		}
+		return nullptr;
+	}
 };
