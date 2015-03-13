ef04758f651c4199df21f1615b7f375983de038d
atymisk <anthoc2@uci.edu>
2015-03-13 01:18:17 -0700

    added in a private variable pointer to AI object

--- 330Lefties/330Lefties/BasicNPC.h
+++ 330Lefties/330Lefties/BasicNPC.h
@@ -5,14 +5,18 @@ class BasicNPC : public NPC
 {
 public:
 	std::string currentseq = "";
-	BasicNPC(Sprite* sprite,std::string h);
+	BasicNPC(Sprite* sprite, std::string h);
 	~BasicNPC();
+	void move(int x, int y);
+
 	void update();
 	void render();
 	void handleEvent(Uint32 e);
+
 
 private:
 	int x;
 	int y;
 	Sprite* s;
+	AI* ai;
 };
