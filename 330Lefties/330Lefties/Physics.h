382553f9bcaa8ab5a7f59c9dce30e528dde920de
erikgllgs <edgalleg@uci.edu>
2015-03-13 17:09:22 -0700

    Update Physics.h

--- 330Lefties/330Lefties/Physics.h
+++ 330Lefties/330Lefties/Physics.h
@@ -138,7 +138,7 @@ int magnitude(Unit* obj){
 	}
 
 void gravity(Unit* obj){
-	obj->Velocity.y += (log10(magnitude(obj)))/1.5;
+	obj->velocity.y += (log10(magnitude(obj)))/1.5;
 	}
 
 void update(Unit* obj){
